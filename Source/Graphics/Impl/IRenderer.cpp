9545c222ead13da80621132ab7d14549c95f83f2
17seannnn <17seannnn@gmail.com>
2022-11-19 22:01:00 +0300

    Implement Bresenham's line

--- Source/Graphics/Impl/IRenderer.cpp
+++ Source/Graphics/Impl/IRenderer.cpp
@@ -1,3 +1,115 @@
 #include "IRenderer.h"
 
+void IRenderer::DrawLine(u32* Buffer, i32 Pitch, i32 X1, i32 Y1, i32 X2, i32 Y2, u32 Color)
+{
+    i32f DX, DY, DX2, DY2, XInc, YInc, Error;
+
+    // Compute deltas
+    DX = X2 - X1;
+    DY = Y2 - Y1;
+
+    // Get X/Y increments and positive deltas
+    if (DX >= 0)
+    {
+        XInc = 1;
+    }
+    else
+    {
+        XInc = -1;
+        DX = -DX;
+    }
+
+    if (DY >= 0)
+    {
+        YInc = Pitch;
+    }
+    else
+    {
+        YInc = -Pitch;
+        DY = -DY;
+    }
+
+    DX2 = DX << 1;
+    DY2 = DY << 1;
+
+    // Compute buffer start
+    Buffer += Y1 * Pitch + X1;
+
+    if (DX > DY)
+    {
+        Error = DY2 - DX;
+
+        for (i32f I = 0; I < DX; ++I)
+        {
+            *Buffer = Color;
+
+            if (Error >= 0)
+            {
+                Error -= DX2;
+                Buffer += YInc;
+            }
+
+            Error += DY2;
+            Buffer += XInc;
+        }
+    }
+    else
+    {
+        Error = DX2 - DY;
+
+        for (i32f I = 0; I < DY; ++I)
+        {
+            *Buffer = Color;
+
+            if (Error >= 0)
+            {
+                Error -= DY2;
+                Buffer += XInc;
+            }
+
+            Error += DX2;
+            Buffer += YInc;
+        }
+    }
+}
+
+void IRenderer::DrawLineSlow(u32* Buffer, i32 Pitch, i32 X1, i32 Y1, i32 X2, i32 Y2, u32 Color)
+{
+    if (Math.Abs(X2 - X1) > Math.Abs(Y2 - Y1))
+    {
+        i32 T;
+        if (X1 > X2)
+        {
+            SWAP(X1, X2, T);
+            SWAP(Y1, Y2, T);
+        }
+
+        const f32 M = (f32)(Y2 - Y1) / (f32)(X2 - X1);
+        const f32 B = Y1 - X1 * M;
+
+        for (i32 X = X1; X < X2; ++X)
+        {
+            f32 Y = M * (f32)X + B;
+            Buffer[(i32f)Y * Pitch + X] = Color;
+        }
+    }
+    else
+    {
+        i32 T;
+        if (Y1 > Y2)
+        {
+            SWAP(X1, X2, T);
+            SWAP(Y1, Y2, T);
+        }
+
+        const f32 M = (f32)(X2 - X1) / (f32)(Y2 - Y1);
+        const f32 B = X1 - Y1 * M;
+
+        for (i32 Y = Y1; Y < Y2; ++Y)
+        {
+            f32 X = M * (f32)Y + B;
+            Buffer[Y * Pitch + (i32f)X] = Color;
+        }
+    }
+}
 
