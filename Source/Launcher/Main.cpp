571eb3b78d19beed4b7e9d5127a2e79ac075a4a3
SnowfallEagle <engineserega@gmail.com>
2023-05-05 12:14:03 +0300

    Some beauty to launcher ui

--- Source/Launcher/Main.cpp
+++ Source/Launcher/Main.cpp
@@ -55,42 +55,52 @@ private:
     bool bBackfaceRemoval = true;
     bool bSortPolygons = false;
 
+public:
+    GLauncher() : VLauncher(L"Launcher", { 800, 600 })
+    {}
+
 private:
     virtual void Update() override
     {
         const ImGuiStyle& Style = ImGui::GetStyle();
-        const ImGuiViewport* Viewport = ImGui::GetMainViewport();
 
         ImGui::Begin("Main Window", nullptr,
             ImGuiWindowFlags_NoMove |
             ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDecoration
         );
 
-        ImGui::SetWindowPos(Viewport->Pos);
-        ImGui::SetWindowSize(Viewport->Size);
-
-        const ImVec2 WorkSize = { Viewport->Size.x / 4, Viewport->Size.x / 4 };
-        const ImVec2 WorkPosition = { (Viewport->Size.x - WorkSize.x) / 2, (Viewport->Size.y - WorkSize.y) / 2 };
-        ImGui::SetCursorPos(WorkPosition);
+        const ImVec2 WindowSize = ImGui::GetWindowSize();
+        const ImVec2 WorkSize = { WindowSize.x / 4, WindowSize.y / 2 };
+        ImGui::SetCursorPos({ (WindowSize.x - WorkSize.x) / 4, (WindowSize.y - WorkSize.y) / 2 });
 
         ImGui::BeginChild("Work", WorkSize);
         {
-            ImGui::Combo("Window Size", &WindowSizeCurrent, WindowSizeItems, VLN_ARRAY_SIZE(WindowSizeItems));
-            ImGui::Combo("Window Type", &WindowTypeCurrent, WindowTypeItems, VLN_ARRAY_SIZE(WindowTypeItems));
+            ImGui::SetCursorPosX(ImGui::GetContentRegionAvail().x / 2 - 100.0f);
+            ImGui::BeginChild("Input Stuff", { 0, 112 });
+            {
+                ImGui::Combo("Window Size", &WindowSizeCurrent, WindowSizeItems, VLN_ARRAY_SIZE(WindowSizeItems));
+                ImGui::Combo("Window Type", &WindowTypeCurrent, WindowTypeItems, VLN_ARRAY_SIZE(WindowTypeItems));
 
-            ImGui::InputInt("FPS Limit", &FPSLimit);
-            ImGui::InputInt("Fixed FPS", &TargetFixedFPS);
-            ImGui::InputInt("Max Mip Maps", &MaxMipMaps);
+                ImGui::InputInt("FPS Limit", &FPSLimit);
+                ImGui::InputInt("Fixed FPS", &TargetFixedFPS);
+                ImGui::InputInt("Max Mip Maps", &MaxMipMaps);
+            }
+            ImGui::EndChild();
 
-            ImGui::Checkbox("Limit FPS", &bLimitFPS);
-            ImGui::Checkbox("Backface Removal", &bBackfaceRemoval);
-            ImGui::Checkbox("Polygons Sorting", &bSortPolygons);
+            ImGui::SetCursorPos({ ImGui::GetContentRegionAvail().x / 2 - 75.0f, ImGui::GetCursorPosY() + 10.0f });
+            ImGui::BeginGroup();
+            {
+                ImGui::Checkbox("Limit FPS", &bLimitFPS);
+                ImGui::Checkbox("Backface Removal", &bBackfaceRemoval);
+                ImGui::Checkbox("Polygons Sorting", &bSortPolygons);
+            }
+            ImGui::EndGroup();
 
             static constexpr const char* RunButtonText = "Run";
             const f32 Size = ImGui::CalcTextSize(RunButtonText).x + Style.FramePadding.x * 2.0f;
             const f32 Available = ImGui::GetContentRegionAvail().x;
 
-            ImGui::SetCursorPosX((Available - Size) / 2);
+            ImGui::SetCursorPos({ (Available - Size) / 2, ImGui::GetCursorPosY() + 10.0f });
             if (ImGui::Button(RunButtonText))
             {
                 static constexpr i32f BufferSize = 512;
@@ -109,6 +119,7 @@ private:
                 {
                     // @TODO: Put this in Volition System class
                     // @TODO: Open launcher after exiting game if was opened through it
+                    // @TODO: Put all commands in separate header in engine
 
                     STARTUPINFOA StartUpInfo;
                     ZeroMemory(&StartUpInfo, sizeof(StartUpInfo));
